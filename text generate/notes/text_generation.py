c0309607ef259e86451ba4c3e670fc2014d36b20
lplpl123 <758826560@qq.com>
2023-05-27 10:24:23 +0800

    202305271024

--- text generate/notes/text_generation.py
+++ text generate/notes/text_generation.py
@@ -39,5 +39,31 @@ examples_per_epoch = len(text)//seq_length
 
 # 创建训练样本 / 目标
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int) # length: 1155388, 对应的就是text的文本数量，每一个都是tensor
+# for i in char_dataset.take(5):
+#     print(idx2char[i.numpy()])
 
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True) # 101 * 11439
+# for item in sequences.take(5):
+#     print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# for input_example, target_example in  dataset.take(1):
+#   print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+#   print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+# for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+#     print("Step {:4d}".format(i))
+#     print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+#     print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+# 批大小
+BATCH_SIZE = 64
+
+# 设定缓冲区大小
 
