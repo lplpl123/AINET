942fe6f3b0e6c13c87e0ab6028a0e088c6f983c3
lplpl123 <758826560@qq.com>
2023-06-03 15:47:42 +0800

    202306031547

--- AI play game/notes/CartPole.py
+++ AI play game/notes/CartPole.py
@@ -4,9 +4,10 @@ for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
         env.render()
-        print(observation)
+        print(env.x_threshold)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
+        print(observation[0])
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
--- AI play game/train.py
+++ AI play game/train.py
@@ -10,18 +10,21 @@ if __name__ == "__main__":
     dqn = dqn.DQN()
 
     for epoch in range(EPOCHES):
-        s = env.reset()
+        s = env.reset() # 把游戏环境初始化，就是把小车放到中间
         # while True:
         for _ in range(100):
             # 可视化展示环境
             env.render()
+
             print("---输入当前状态---", s)
             action_value = dqn.choose_action(s) # action: tensor([0.0868, 0.1454], grad_fn=<AddBackward0>)
             print("---输出当前动作---", action_value)
             action = torch.argmax(action_value).numpy()
             y_eval = action_value[int(action)]
             next_s, reward, done, info = env.step(action)
-
+            x, x_dot, theta, theta_dat = next_s
+            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            r2 = (env.theta_threshold_radians)
             next_action_value = dqn.choose_action(next_s)
             dqn.learn(next_action_value, reward, y_eval)
 
